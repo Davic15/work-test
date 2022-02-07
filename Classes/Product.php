464cba4368fca9bae7515019c2c511910d6b8b1a
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-02-07 16:57:55 +0100

    Added class product and DVD (beta)

---
+++ Classes/Product.php
@@ -0,0 +1,15 @@
+<?php
+    abstract class Product {
+        public $name;
+        public $sku;
+        public $price;
+        public $productType;
+        public function __construct($data) {
+            $this->name = $data["name"];
+            $this->sku = $data["sku"];
+            $this->price = $data["price"];
+            $this->producType = $data["productType"];
+        }
+        abstract function addProduct();
+    }
+?>
