c5bbc68e4674f72b9e647aca8324b88919025a84
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-02-07 15:02:20 +0100

    Added index and CSS (partially)

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
-<?php
-
-// Show all information, defaults to INFO_ALL
-phpinfo();
-
-// Show just the module information.
-// phpinfo(8) yields identical results.
-phpinfo(INFO_MODULES);
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./css/style.css">
+        <title>Scandiweb Test</title>
+    </head>
+    <body>
+        <nav>
+            <div class="container-nav">
+                <h1>Product List</h1>
+                <div class="container-nav_buttons">
+                    <button class="btn add">ADD</button>
+                    <button class="btn delete">MASS DELETE</button>
+                </div>
+            </div>
+        </nav>
+    </body>
+</html>
